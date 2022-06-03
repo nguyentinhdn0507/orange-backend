23b6103b702c92ed13e00c1979ef8e5020be0053
nguyentinhdn0507 <69786007+nguyentinhdn0507@users.noreply.github.com>
2022-06-03 11:48:23 +0700

    re-deploy

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,77 +1,77 @@
-const {
-  findByUserNameRepo,
-  findUserByIdRepo,
-  findAlUserRepo,
-  AddUserRepo,
-  UpdateUserRepo,
-  deleteUserRepo,
-  findUserRepo,
-} = require("../repository/UserRepository");
+// const {
+//   findByUserNameRepo,
+//   findUserByIdRepo,
+//   findAlUserRepo,
+//   AddUserRepo,
+//   UpdateUserRepo,
+//   deleteUserRepo,
+//   findUserRepo,
+// } = require("../repository/UserRepository");
 const md5 = require("md5");
 const { throwToken, generateRefreshToken } = require("../authen/auth");
 
 async function loginService(username, password) {
-  const login = await findUserRepo(username, md5(password));
-  console.log("login", login);
-  if (login) {
-    const { _id, username } = login;
-    await UpdateUserRepo(_id, {
-      refreshToken: generateRefreshToken({ _id, username }),
-    });
-    const result = throwToken({ _id, username });
-    return result;
-  } else {
-    return;
-  }
+  // const login = await findUserRepo(username, md5(password));
+  // console.log("login", login);
+  // if (login) {
+  //   const { _id, username } = login;
+  //   await UpdateUserRepo(_id, {
+  //     refreshToken: generateRefreshToken({ _id, username }),
+  //   });
+  //   const result = throwToken({ _id, username });
+  //   return result;
+  // } else {
+  //   return;
+  // }
 }
 function findOneService(id) {
-  const findUser = findUserByIdRepo(id);
-  console.log("login", findUser);
-  if (findUser) {
-    return findUser;
-  } else {
-    return;
-  }
+  // const findUser = findUserByIdRepo(id);
+  // console.log("login", findUser);
+  // if (findUser) {
+  //   return findUser;
+  // } else {
+  //   return;
+  // }
 }
 //Get All
 function showAllUserService() {
-  const showAllUser = findAlUserRepo();
-  if (showAllUser) {
-    return showAllUser;
-  } else {
-    return;
-  }
+  // const showAllUser = findAlUserRepo();
+  // if (showAllUser) {
+  //   return showAllUser;
+  // } else {
+  //   return;
+  // }
 }
 // Register
 function registerService(user) {
-  const haveUserName = findByUserNameRepo(user.username);
-  if (!haveUserName) {
-    const newUser = {
-      username: user.username,
-      password: md5(user.password),
-    };
-    const result = AddUserRepo(newUser);
-    if (result) {
-      return result;
-    } else {
-      return;
-    }
-  } else {
-    return { Message: "Tên Đăng Nhập Đã Tồn Tại" };
-  }
+  // const haveUserName = findByUserNameRepo(user.username);
+  // if (!haveUserName) {
+  //   const newUser = {
+  //     username: user.username,
+  //     password: md5(user.password),
+  //   };
+  //   const result = AddUserRepo(newUser);
+  //   if (result) {
+  //     return result;
+  //   } else {
+  //     return;
+  //   }
+  // } else {
+  //   return { Message: "Tên Đăng Nhập Đã Tồn Tại" };
+  // }
 }
 //Update
 function updateUserService(id, user) {
-  return UpdateUserRepo(id, user);
+  // return UpdateUserRepo(id, user);
   // console.log("update", update);
 }
 // delete
 function deleteUserService(id) {
-  const deleteUser = deleteUserRepo(id);
-  if (deleteUser.deletedCount == 0) {
-    return;
-  }
-  return deleteUser;
+  // const deleteUser = deleteUserRepo(id);
+  // if (deleteUser.deletedCount == 0) {
+  //   return;
+  // }
+  // return deleteUser;
 }
 module.exports = {
   loginService,
